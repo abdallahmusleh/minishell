43a69a31c700e9a3f8f60864656f59432c7e0da3
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-02-05 00:22:25 +0300

    Refactor lexer functions for better readability

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,196 +6,61 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 21:29:09 by abmusleh          #+#    #+#             */
-/*   Updated: 2026/02/02 23:36:57 by abmusleh         ###   ########.fr       */
+/*   Updated: 2026/02/05 00:19:11 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/lexer.h"
 #include "libft/libft.h"
+#include "minishell.h"
 
-static int space_finder(char c)
+static int	handle_operator(t_token **token_list, char *line, int *i)
 {
-    if (c == 32 || (c >= 9 && c <= 13))//check what counts as empty space
-        return (1);
-    else
-        return (0);
+	if (line[*i] == '<')
+		return (handle_redirect_in(token_list, line, i));
+	else if (line[*i] == '>')
+		return (handle_redirect_out(token_list, line, i));
+	else if (line[*i] == '|')
+        return( handle_pipe(token_list, i));
+	return (1);
 }
-// static t_token word_parser(char *line, int *i)
-// {
-//     //check what to do for commands with spaces in between that arent in between quotations
-//     char *word;
-//     int tmp;
-//     int quotation_counter;
-
-//     tmp = *i + 1;
-//     quotation_counter = 0;
-//     if (line[*i] == '\'' || line[*i] == '"')//check how to see if current character is a quotation
-//     {
-//         while (line[tmp])
-//         {
-//             if (line[tmp] == line[*i])
-//                 quotation_counter++;
-//             tmp++;
-//         }
-//         if (quotation_counter == 0)
-//             return(error unclosed quote);//check if this is correct return
-//         tmp = *i + 1;
-//         while (line[tmp] && line[tmp] != line[*i])
-//             tmp++;
-//         word = ft_substr(line, *i + 1, tmp - *i - 1);
-//         if (!word)
-//             return(NULL);
-//         *i = tmp + 1;
-//         return(token_list_new(WORD, word));
-//     }
-//     while (line[tmp] && line[tmp] != '<' && line[tmp] != '>' && line[tmp] != '|' && !space_finder(line[tmp]))
-//         tmp++;
-//     word = ft_substr(line, *i, tmp - *i);
-//     if (!word)
-//         return(NULL);
-//     *i = tmp;
-//     return(token_list_new(WORD, word));
-// }
-// static t_token word_parser(char *line, int *i)
-// {
-//     char *word;
-//     int tmp;
-
-//     tmp = *i //+ 1;
-//     while (line[tmp])
-//     {
-//         if (space_finder(line[tmp]) || line[tmp] == '|' ||
-//             line[tmp] == '<' || line[tmp] == '>')
-//             break;
-//     if (line[tmp] == '\'' || line[tmp] == '"')//check how to see if current character is a quotation
-//     {
-//         tmp++;
-//         while (line[tmp] && line[tmp] != line[*i])
-//             tmp++;
-//         if (!line[tmp])
-//             return(error unclosed quote);
-//         // word = ft_substr(line, *i + 1, tmp - *i - 1);
-//         // if (!word)
-//         //     return(NULL);
-//         // return(token_list_new(WORD, word));
-//     }
-//     else
-//         tmp++;
-//     word = ft_substr(line, *i, tmp - *i);
-//     if (!word)
-//         return(NULL);
-//     *i = tmp + 1;
-//     return(remove_quotes(token_list_new(WORD, word)));
-//     }
-// }
-char *substr_and_quotes_remover(char *line, int start, int *i)
+static int	handle_word(t_token **token_list, int *i, char *line)
 {
-    char *newstr;
-    char *word;
-    int x;
-    int j;
+	t_token	*token;
 
-    x = 0;
-    j = 0;
-    word = ft_substr(line, start, *i - start);
-    if (!word)
-        return (NULL);
-    newstr = malloc(ft_strlen(word) + 1);
-    if (!newstr)
-    {
-        free(word);
-        return (NULL);
-    }
-    while (word[x])
-    {
-        if (word[x] != '\'' && word[x] != '"')
-            newstr[j++] = word[x];
-        x++;
-    }
-    newstr[j] = '\0';
-    free(word);
-    return (newstr);
+	token = word_parser(line, i);
+	if (!token)
+	{
+		lst_clear(token_list);
+		return (0);
+	}
+	ft_lstadd_back(token_list, token);
+	return (1);
 }
-t_token *word_parser(char *line, int *i)
+t_token	*lexing(char *line)
 {
-    int start;
-    char quote;
-    char *word;
+	t_token	*token_list;
+	int		i;
 
-    start = *i;
-    while (line[*i])
-    {
-        if (space_finder(line[*i]) || line[*i] == '|' ||
-            line[*i] == '<' || line[*i] == '>')
-            break;
-        if (line[*i] == '\'' || line[*i] == '"')
-        {
-            quote = line[*i];
-            (*i)++;
-            while (line[*i] && line[*i] != quote)
-                (*i)++;
-            if (!line[*i])
-                return (NULL);
-            (*i)++;
-        }
-        else
-            (*i)++;
-    }
-    return (token_list_new(WORD, substr_and_quotes_remover(line, start, i)));
+	if (!line || !*line)
+		return (NULL);
+	token_list = NULL;
+	i = 0;
+	while (line[i])
+	{
+		if (space_finder(line[i]))
+			i++;
+		else if (line[i] == '<' || line[i] == '>' || line[i] == '|')
+		{
+			if (!handle_operator(&token_list, line, &i))
+				return (NULL);
+		}
+		else
+		{
+			if (!handle_word(&token_list, &i, line))
+				return (NULL);
+		}
+	}
+	return (token_list);
 }
-
-
-t_token *lexing(char *line)
-{
-    t_token *token_list;
-    int i;
-
-    token_list = NULL;
-    i = 0;
-    while (line[i])
-    {
-        if (space_finder(line[i]))
-            i++;
-        else if (line[i] == '<')
-        {
-            if (line[i + 1] && line[i + 1] == '<')
-            {
-                ft_lstadd_back(&token_list, token_list_new(HEREDOC, "<<"));
-                i = i + 2;
-            }
-            else
-            {
-                ft_lstadd_back(&token_list, token_list_new(REDIR_IN, "<"));
-                i++;
-            }
-        }
-        else if (line[i] == '>')
-        {
-            if (line[i + 1] && line[i + 1] == '>')
-            {
-                ft_lstadd_back(&token_list, token_list_new(REDIR_APPEND, ">>"));
-                i = i + 2;
-            }
-            else
-            {
-                ft_lstadd_back(&token_list, token_list_new(REDIR_OUT, ">"));
-                i++;
-            }
-        }
-        else if (line[i] == '|')
-        {
-            ft_lstadd_back(&token_list, token_list_new(PIPE, "|"));
-            i++;
-        }
-        else
-        {
-            t_token *tok = word_parser(line, &i);
-            if (tok)
-                ft_lstadd_back(&token_list, tok);
-            else
-                lst_clear(token_list);
-                return(NULL);
-        }
-    }
-    return(token_list);
-}
+//ask about end of file token/ error token
