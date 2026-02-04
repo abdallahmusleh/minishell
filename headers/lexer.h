1ef60488cec6bb052a2f58f579a8a64cafeb59a7
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-02-05 00:21:54 +0300

    Update lexer.h with new function declarations

--- headers/lexer.h
+++ headers/lexer.h
@@ -6,7 +6,7 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 21:29:28 by abmusleh          #+#    #+#             */
-/*   Updated: 2026/02/02 23:22:19 by abmusleh         ###   ########.fr       */
+/*   Updated: 2026/02/05 00:05:12 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,13 @@ typedef struct s_token
 }   t_token;
 
 t_token *lexing(char *line);
+t_token *token_list_new(t_token_type type, char *value);
+void	ft_lstadd_back(t_token **list, t_token *new);
+void	lst_clear(t_token **list);
+int		space_finder(char c);
+t_token	*word_parser(char *line, int *i);
+int		handle_redirect_in(t_token **token_list, char *line, int *i);
+int		handle_redirect_out(t_token **token_list, char *line, int *i);
+int		handle_pipe(t_token **token_list, int *i);
 
 #endif
