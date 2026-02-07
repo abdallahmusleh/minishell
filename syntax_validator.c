a0d65df7adab394559fe8557743d5251e1d2c399
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-02-07 13:08:26 +0300

    Add files via upload

---
+++ syntax_validator.c
@@ -0,0 +1,53 @@
+#include "headers/minishell.h"
+#include "libft/libft.h"
+
+static int pipe_validator(t_token *tokens)
+{
+    t_token *tmp_tokens;
+
+    tmp_tokens = tokens;
+    if (tmp_tokens->type == PIPE)
+        return (0);
+    while (tmp_tokens && tmp_tokens->type != T_EOF)
+    {
+        if (tmp_tokens->type == PIPE)
+        {
+            if (tmp_tokens->next && tmp_tokens->next->type == T_EOF)
+                return(0);
+            if (tmp_tokens->next && tmp_tokens->next->type == PIPE)
+                return(0);
+        }
+        tmp_tokens = tmp_tokens->next;
+    }
+    return (1);
+}
+
+static int redirections_validator(t_token *tokens)
+{
+    t_token *tmp_tokens;
+
+    tmp_tokens = tokens;
+    while (tmp_tokens && tmp_tokens->type != T_EOF)
+    {
+        if (tmp_tokens->type == REDIR_IN || tmp_tokens->type == REDIR_OUT
+            || tmp_tokens->type == REDIR_APPEND || tmp_tokens->type == HEREDOC)
+        {
+            if (!tmp_tokens->next || tmp_tokens->next->type != WORD)
+                return(0);
+            //check if i need to handle multiple conflicting redirections ex: cmd < file1 < file2
+        }
+        tmp_tokens = tmp_tokens->next;
+    }
+    return(1);
+}
+
+int syntax_validator(t_token *tokens)
+{
+    if (!tokens || tokens->type == T_EOF)
+        return (1);
+    if (!pipe_validator(tokens))
+        return (0);
+    if (!redirections_validator(tokens))
+        return (0);
+    return (1);
+}
