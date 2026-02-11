d45b95b5add42f38540a950802a3ac964ba14724
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-02-11 21:09:26 +0300

    Fix invalid fd error message length

--- parsing_redirectors.c
+++ parsing_redirectors.c
@@ -6,7 +6,7 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:56:46 by abmusleh          #+#    #+#             */
-/*   Updated: 2026/02/11 16:33:43 by abmusleh         ###   ########.fr       */
+/*   Updated: 2026/02/11 19:40:31 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static int  parse_redir_in(t_pipeline *pipeline, t_token **token, int i)
     fd = open((*token)->next->value, O_RDONLY);
     if (fd == -1)
     {
-        write(2, "invalid fd", 11);
+        write(2, "invalid fd", 10);
         return(0);    
     }
     pipeline->commands[i].input_fd = fd;
@@ -40,7 +40,7 @@ static int  parse_redir_out(t_pipeline *pipeline, t_token **token, int i)
     fd = open((*token)->next->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1)
     {
-        write(2, "invalid fd", 11);
+        write(2, "invalid fd", 10);
         return(0);    
     }
     pipeline->commands[i].output_fd = fd;
@@ -57,7 +57,7 @@ static int  parse_redir_append(t_pipeline *pipeline, t_token **token, int i)
     fd = open((*token)->next->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (fd == -1)
     {
-        write(2, "invalid fd", 11);
+        write(2, "invalid fd", 10);
         //check actual error msg
         return(0);    
     }
@@ -65,21 +65,7 @@ static int  parse_redir_append(t_pipeline *pipeline, t_token **token, int i)
     *token = (*token)->next;
     return(1);
 }
-void    parse_heredoc_helper(int fd[2], char *str)
-{
-    close(fd[0]);
-    close(fd[1]);
-    write(2,
-		"warning: here-document delimited by end-of-file (wanted `",
-		58);
-	write(2, str, ft_strlen(str));
-	write(2, "')\n", 3);
-}
-void    free_and_close(char *str, int fd)
-{
-    free(str);
-    close(fd);
-}
+
 static int parse_heredoc(t_pipeline *pipeline, t_token **tokens, int i)
 {
     int fd[2];
@@ -87,7 +73,7 @@ static int parse_heredoc(t_pipeline *pipeline, t_token **tokens, int i)
     
     if (pipe(fd) == -1)
 	{
-        write (2, "pipe error", 11);
+        write (2, "pipe error", 10);
         return (0);
     }
     while (1)
@@ -95,21 +81,27 @@ static int parse_heredoc(t_pipeline *pipeline, t_token **tokens, int i)
         str = readline(">");
         if (!str)
         {
-            parse_heredoc_helper(fd, str);
-            return(0);
+            parse_heredoc_helper(fd, (*tokens)->next->value);
+            break ;
         }
         if (!ft_strcmp(str, (*tokens)->next->value))
-            break;
+        {
+            free(str);
+            break ;
+        }
         write(fd[1], str, ft_strlen(str));
         write(fd[1], "\n", 1);
         free(str);
     }
-    free_and_close(str, fd[1]);
+    close(fd[1]);
+    if (pipeline->commands[i].input_fd != STDIN_FILENO)
+        close(pipeline->commands[i].input_fd);
+    pipeline->commands[i].input_fd = fd[0];
     *tokens = (*tokens)->next;
-    return(fd[0]);
+    return(1);
 }
 
-static int parse_redir(t_pipeline *pipeline, t_token **tmp_tokens, int i)
+int parse_redir(t_pipeline *pipeline, t_token **tmp_tokens, int i)
 {
     if ((*tmp_tokens)->type == REDIR_IN)
         return(parse_redir_in(pipeline, tmp_tokens, i));
