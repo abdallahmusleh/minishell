771c5fe13ee0fa92f5a8de6af9ab8573d3ba9208
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-03-26 16:08:18 +0300

    Refactor memory management in parsing_utils.c

--- parsing_utils.c
+++ parsing_utils.c
@@ -6,70 +6,62 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:59:52 by abmusleh          #+#    #+#             */
-/*   Updated: 2026/02/11 19:16:19 by abmusleh         ###   ########.fr       */
+/*   Updated: 2026/02/15 19:34:47 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/parsing.h"
 #include "headers/minishell.h"
 
-void free_pipeline (t_pipeline *pipeline)
+static void	free_args(char **args)
 {
-    int i;
-    int j;
-    
-    if (!pipeline)
-        return;
-    i = 0;
-    if (pipeline->commands)
-    {
-        while (i < pipeline->num_commands)
-        {
-            if (pipeline->commands[i].args)
-            {
-                j = 0;
-                while (pipeline->commands[i].args[j])
-                {
-                    free(pipeline->commands[i].args[j]);
-                    j++;
-                }
-                free(pipeline->commands[i].args);
-            }
-            if (pipeline->commands[i].input_fd != STDIN_FILENO)
-                close(pipeline->commands[i].input_fd);
-            if (pipeline->commands[i].output_fd != STDOUT_FILENO)
-                close(pipeline->commands[i].output_fd);
-            i++;
-        }
-        free(pipeline->commands);
-    }
-    free(pipeline);
+	int	j;
+
+	if (!args)
+		return ;
+	j = 0;
+	while (args[j])
+	{
+		free(args[j]);
+		j++;
+	}
+	free(args);
 }
 
-int command_counter(t_token *tokens)
+void	free_pipeline(t_pipeline *pipeline)
 {
-    int i;
-    
-    if (!tokens)
-        return (0);
-    i = 1;
-    while (tokens && tokens->type != T_EOF)
-    {
-        if (tokens->type == PIPE)
-            i++;
-        tokens = tokens->next;
-    }
-    return(i);
+	int	i;
+
+	if (!pipeline)
+		return ;
+	i = 0;
+	while (pipeline->commands && i < pipeline->num_commands)
+	{
+		free_args(pipeline->commands[i].args);
+		if (pipeline->commands[i].input_fd != STDIN_FILENO)
+			close(pipeline->commands[i].input_fd);
+		if (pipeline->commands[i].output_fd != STDOUT_FILENO)
+			close(pipeline->commands[i].output_fd);
+		i++;
+	}
+	free(pipeline->commands);
+	free(pipeline);
 }
 
-void    parse_heredoc_helper(int fd[2], char *str)
+int	command_counter(t_token *tokens)
 {
-    write(2,
-		"warning: here-document delimited by end-of-file (wanted `",
-		57);
-    if (str)
-        write(2, str, ft_strlen(str));
-	write(2, "')\n", 3);
+	int	i;
+
+	if (!tokens)
+		return (0);
+	i = 1;
+	while (tokens && tokens->type != T_EOF)
+	{
+		if (tokens->type == PIPE)
+			i++;
+		tokens = tokens->next;
+	}
+	return (i);
 }
 
 void    free_and_close(char *str, int fd)
