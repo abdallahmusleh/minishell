cbc1ee1acc627d9b098a932fd457d42ecd26fe6e
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2026-03-26 16:09:11 +0300

    Refactor redirection parsing functions

--- parsing_redirectors.c
+++ parsing_redirectors.c
@@ -6,7 +6,7 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:56:46 by abmusleh          #+#    #+#             */
-/*   Updated: 2026/02/11 19:40:31 by abmusleh         ###   ########.fr       */
+/*   Updated: 2026/02/15 18:35:33 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,102 +14,82 @@
 #include "headers/parsing.h"
 #include "libft/libft.h"
 
-static int  parse_redir_in(t_pipeline *pipeline, t_token **token, int i)
+static int	redir_target_ok(t_token *token)
 {
-    int fd;
+	if (!token || !token->next || token->next->type != WORD)
+	{
+		write(2, "syntax error near unexpected token `newline'\n", 45);
+		return (0);
+	}
+	return (1);
+}
 
+static int	parse_redir_in(t_pipeline *pipeline, t_token **token, int i)
+{
+    int	fd;
+
+    if (!redir_target_ok(*token))
+        return (0);
     if (pipeline->commands[i].input_fd != STDIN_FILENO)
         close(pipeline->commands[i].input_fd);
     fd = open((*token)->next->value, O_RDONLY);
     if (fd == -1)
     {
         write(2, "invalid fd", 10);
-        return(0);    
+        return (0);
     }
     pipeline->commands[i].input_fd = fd;
     *token = (*token)->next;
-    return(1);
+    return (1);
 }
 
-static int  parse_redir_out(t_pipeline *pipeline, t_token **token, int i)
+static int	parse_redir_out(t_pipeline *pipeline, t_token **token, int i)
 {
-    int fd;
+    int	fd;
 
+    if (!redir_target_ok(*token))
+        return (0);
     if (pipeline->commands[i].output_fd != STDOUT_FILENO)
         close(pipeline->commands[i].output_fd);
     fd = open((*token)->next->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1)
     {
         write(2, "invalid fd", 10);
-        return(0);    
+        return (0);
     }
     pipeline->commands[i].output_fd = fd;
     *token = (*token)->next;
-    return(1);
+    return (1);
 }
 
-static int  parse_redir_append(t_pipeline *pipeline, t_token **token, int i)
+static int	parse_redir_append(t_pipeline *pipeline, t_token **token, int i)
 {
-    int fd;
+    int	fd;
 
+    if (!redir_target_ok(*token))
+        return (0);
     if (pipeline->commands[i].output_fd != STDOUT_FILENO)
         close(pipeline->commands[i].output_fd);
     fd = open((*token)->next->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (fd == -1)
     {
         write(2, "invalid fd", 10);
-        //check actual error msg
-        return(0);    
+        return (0);
     }
     pipeline->commands[i].output_fd = fd;
     *token = (*token)->next;
-    return(1);
+    return (1);
 }
 
-static int parse_heredoc(t_pipeline *pipeline, t_token **tokens, int i)
-{
-    int fd[2];
-    char *str;
-    
-    if (pipe(fd) == -1)
-	{
-        write (2, "pipe error", 10);
-        return (0);
-    }
-    while (1)
-    {
-        str = readline(">");
-        if (!str)
-        {
-            parse_heredoc_helper(fd, (*tokens)->next->value);
-            break ;
-        }
-        if (!ft_strcmp(str, (*tokens)->next->value))
-        {
-            free(str);
-            break ;
-        }
-        write(fd[1], str, ft_strlen(str));
-        write(fd[1], "\n", 1);
-        free(str);
-    }
-    close(fd[1]);
-    if (pipeline->commands[i].input_fd != STDIN_FILENO)
-        close(pipeline->commands[i].input_fd);
-    pipeline->commands[i].input_fd = fd[0];
-    *tokens = (*tokens)->next;
-    return(1);
-}
-
-int parse_redir(t_pipeline *pipeline, t_token **tmp_tokens, int i)
+int	parse_redir(t_pipeline *pipeline, t_token **tmp_tokens, int i)
 {
     if ((*tmp_tokens)->type == REDIR_IN)
-        return(parse_redir_in(pipeline, tmp_tokens, i));
+        return (parse_redir_in(pipeline, tmp_tokens, i));
     else if ((*tmp_tokens)->type == REDIR_OUT)
-        return(parse_redir_out(pipeline, tmp_tokens, i));
+        return (parse_redir_out(pipeline, tmp_tokens, i));
     else if ((*tmp_tokens)->type == REDIR_APPEND)
-       return(parse_redir_append(pipeline, tmp_tokens, i));
+        return (parse_redir_append(pipeline, tmp_tokens, i));
     else if ((*tmp_tokens)->type == HEREDOC)
-        return(parse_heredoc(pipeline, tmp_tokens, i));
-    return(1);
+        return (parse_heredoc(pipeline, tmp_tokens, i));
+    return (1);
 }
